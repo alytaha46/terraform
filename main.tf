6fa090ec98f77d6f4802b2a99fb4f5ffc3fbba97
Aly Taha <alytaha46@gmail.com>
2023-05-20 22:32:03 +0300

    create vpc, public subnet, ec2 has apache

--- main.tf
+++ main.tf
@@ -1,7 +1,85 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_vpc" "first_vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "link_route_table" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_security_group" "ssh_http_security_group" {
+  name        = "ssh_http_security_group"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+}
+
+resource "aws_instance" "ec2" {
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  ami                         = data.aws_ami.ubuntu.id
+  security_groups             = [aws_security_group.ssh_http_security_group.id]
+  user_data                   = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y apache2
+    systemctl start apache2
+    systemctl enable apache2
+  EOT
 }
